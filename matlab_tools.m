117ff70fd666f0a19e6c2d73dd0bb07c6c9f2341
Hossein <haeri.hsn@gmail.com>
2020-04-21 16:59:58 -0400

    update

--- matlab_tools.m
+++ matlab_tools.m
@@ -2,14 +2,16 @@
       0.8500, 0.3250, 0.0980;
       0.9290, 0.6940, 0.1250;
       0.4660, 0.6740, 0.1880];
-
-
+% t = linspace(0,1,1000);
+% raw = [t ;rand(1,1000)];
+raw = rand(1,1000);
 %% PLOT MEAN +/- STANDARD DEVIATION %%
-n = 100 % window length
-avg = nanmean(reshape([raw; nan(mod(-numel(raw),n),1)],n
-std = nanstd(reshape([raw; nan(mod(-numel(raw),n),1)],n,[]));
+avg_win = 20; % window length
+avg = nanmean(reshape([raw; nan(mod(-numel(raw),avg_win),1)],avg_win,[]));
+std = nanstd(reshape([raw; nan(mod(-numel(raw),avg_win),1)],avg_win,[]));
 x = (1:1:numel(avg));
-patch([x fliplr(x)], [avg-std fliplr(avg+std)], color(i,:), 'EdgeColor', [1, 1, 1], 'LineWidth', .1, 'FaceAlpha',.1)
-plot(avg, 'Color', color(i,:), 'LineWidth', 1);
+patch([x fliplr(x)], [avg-std fliplr(avg+std)], color(1,:), 'EdgeColor', [1, 1, 1], 'LineWidth', .1, 'FaceAlpha',.1)
+hold on
+plot(avg, 'Color', color(1,:), 'LineWidth', 1);
 
 
